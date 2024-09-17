db0564d90ddf3abc8342d7312d995b7d55126887
Irina Khameeva <irina.khameeva@gmail.com>
2024-09-17 19:42:25 -0400

    make 2 pages for cold and hot coffee, background image added

--- index.js
+++ index.js
@@ -1,108 +1,85 @@
 const cardCoffee = document.getElementById("card");
 
+function generateTable(targetId, data) {
+  let parent = document.createElement("div");
+ 
+  for (const element of data) {
+    //document.getElementById("coffee-title").innerHTML=element.title;
+    //console.log(elment);
+    let flipCard = document.createElement("div");
+    flipCard.className = "flip-card";
+    let flipCardInner = document.createElement("div");
+    flipCardInner.className = "flip-card-inner";
 
-fetch("https://api.sampleapis.com/coffee/hot")
-   .then((response) => {
-      if (!response.ok) {
-        throw new Error("Request failed");
-      } else {
-        return response.json();
-      }
-   })
-   .then((data) =>{
-      let parent = document.createElement("div");
-     
-      for (const element of data) {
-        //document.getElementById("coffee-title").innerHTML=element.title;
-        //console.log(element);
-        let flipCard = document.createElement("div");
-        flipCard.className = "flip-card";
-        let flipCardInner = document.createElement("div");
-        flipCardInner.className = "flip-card-inner";
+    let flipCardFront = document.createElement("div");
+    flipCardFront.className = "flip-card-front";
 
-        let flipCardFront = document.createElement("div");
-        flipCardFront.className = "flip-card-front";
-
-        let image = document.createElement("img");
-        image.src = element.image;
-        image.style = "width:100%;height:100%;"
+    let image = document.createElement("img");
+    image.src = element.image;
+    image.style = "width:100%;height:100%;"
 
 
-        let coffeTitle= document.createElement("h3");
-        coffeTitle.textContent = element.title;
-        let flipCardBack = document.createElement("div");
-        flipCardBack.className = "flip-card-back";
+    let coffeTitle= document.createElement("h3");
+    coffeTitle.textContent = element.title;
+    let flipCardBack = document.createElement("div");
+    flipCardBack.className = "flip-card-back";
 
-        let p = document.createElement("p");
-        p.textContent = element.description;
-        console.log(element);
-        flipCard.appendChild(flipCardInner);
-        flipCardInner.appendChild(flipCardFront);
-        flipCardFront.appendChild(coffeTitle);
-        flipCardFront.appendChild(image);
-        flipCardInner.appendChild(flipCardBack);
-        flipCardBack.appendChild(p);
-      
-          parent.appendChild(flipCard)
-      }
-      document.getElementById("hot").appendChild(parent)
-   })
+    let p = document.createElement("p");
+    p.textContent = element.description;
+    flipCard.appendChild(flipCardInner);
+    flipCardInner.appendChild(flipCardFront);
+    flipCardFront.appendChild(coffeTitle);
+    flipCardFront.appendChild(image);
+    flipCardInner.appendChild(flipCardBack);
+    flipCardBack.appendChild(p);
+  
+      parent.appendChild(flipCard)
+  }
+  document.getElementById(targetId).appendChild(parent)
+}
 
-   .catch((error) => {
-    console.error("An error occured:", error);
-   });
+if (document.getElementById("hot")!=null) {
 
-
-   fetch("https://api.sampleapis.com/coffee/iced")
-   .then((response) => {
-      if (!response.ok) {
-        throw new Error("Request failed");
-      } else {
-        return response.json();
-      }
-   })
-   .then((data) =>{
-      let parent = document.createElement("div");
-     
-      for (const element of data) {
-        //document.getElementById("coffee-title").innerHTML=element.title;
-        //console.log(element);
-        let flipCard = document.createElement("div");
-        flipCard.className = "flip-card";
-        let flipCardInner = document.createElement("div");
-        flipCardInner.className = "flip-card-inner";
-
-        let flipCardFront = document.createElement("div");
-        flipCardFront.className = "flip-card-front";
-
-        let image = document.createElement("img");
-        image.src = element.image;
-        image.style = "width:100%;height:100%;"
+  fetch("https://api.sampleapis.com/coffee/hot")
+    .then((response) => {
+        if (!response.ok) {
+          throw new Error("Request failed");
+        } else {
+          return response.json();
+        }
+    })
+  .then((data) =>{
+    generateTable("hot", data)
+  
+  })
 
 
-        let coffeTitle= document.createElement("h3");
-        coffeTitle.textContent = element.title;
-        let flipCardBack = document.createElement("div");
-        flipCardBack.className = "flip-card-back";
+  .catch((error) => {
+  console.error("An error occured:", error);
+  });
+}
 
-        let p = document.createElement("p");
-        p.textContent = element.description;
-        console.log(element);
-        flipCard.appendChild(flipCardInner);
-        flipCardInner.appendChild(flipCardFront);
-        flipCardFront.appendChild(coffeTitle);
-        flipCardFront.appendChild(image);
-        flipCardInner.appendChild(flipCardBack);
-        flipCardBack.appendChild(p);
-      
-          parent.appendChild(flipCard)
-      }
-      document.getElementById("cold").appendChild(parent)
+if (document.getElementById("cold")!=null) {
+
+  fetch("https://api.sampleapis.com/coffee/iced")
+  .then((response) => {
+     if (!response.ok) {
+       throw new Error("Request failed");
+     } else {
+       return response.json();
+     }
+  })
+  .then((data) =>{
+   generateTable("cold", data)
    })
 
-   .catch((error) => {
-    console.error("An error occured:", error);
-   });
+  .catch((error) => {
+   console.error("An error occured:", error);
+  }); 
+}
+
+
+
 
 
   
